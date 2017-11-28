bf2da53ba3d146b47e091a93fc1a830c7c848f35
浅爱 <shallowlove21@gmail.com>
2017-11-28 23:51:11 +0800

    Initial Commit

---
+++ Speech/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Speech
+//
+//  Created by PatrickY on 2017/11/28.
+//  Copyright © 2017年 PatrickY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Speech/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Speech
+//
+//  Created by PatrickY on 2017/11/28.
+//  Copyright © 2017年 PatrickY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
